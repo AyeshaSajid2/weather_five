09c2e0f4c3a1079dbecaae686828a1669af1d02e
AyeshaSajid2 <147182897+AyeshaSajid2@users.noreply.github.com>
2024-04-20 20:50:01 +0500

    fetched weather data by city name

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -12,18 +12,17 @@ class _WeatherScreenState extends State<WeatherScreen> {
   final WeatherService _weatherService = WeatherService(apiKey: 'a377581026c124fafded5d390a5d9732');
   late List<List<Weather>> _forecastByDay = [];
   late int _currentPageIndex = 0;
+  TextEditingController _cityController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _fetchWeatherForecast();
+    _fetchWeatherForecast('New York');
   }
 
-  Future<void> _fetchWeatherForecast() async {
-    double lat = 40.7128; // Latitude of New York City
-    double lon = -74.0060; // Longitude of New York City
+  Future<void> _fetchWeatherForecast(String cityName) async {
     try {
-      List<Weather> forecast = await _weatherService.getFiveDayForecastByLocation(lat, lon);
+      List<Weather> forecast = await _weatherService.getFiveDayForecastByCityName(cityName);
       _forecastByDay = _groupForecastByDay(forecast);
       setState(() {});
     } catch (e) {
@@ -80,13 +79,44 @@ class _WeatherScreenState extends State<WeatherScreen> {
     }
   }
 
+  void _onSearch() {
+    String cityName = _cityController.text;
+    if (cityName.isNotEmpty) {
+      _fetchWeatherForecast(cityName);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _forecastByDay.isNotEmpty
-          ? WeatherUI(forecast: _forecastByDay[_currentPageIndex])
-          : Center(
-        child: CircularProgressIndicator(),
+      appBar: AppBar(
+        title: Text('Weather Forecast'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: _onSearch,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _cityController,
+              decoration: InputDecoration(
+                hintText: 'Enter city name',
+              ),
+            ),
+          ),
+          Expanded(
+            child: _forecastByDay.isNotEmpty
+                ? WeatherUI(forecast: _forecastByDay[_currentPageIndex])
+                : Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
